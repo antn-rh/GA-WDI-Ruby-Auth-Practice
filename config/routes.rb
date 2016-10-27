12d6695056236dcd99a5802050c2479d93c72f15
Antonio Rhee <antoniolrhee@gmail.com>
2016-10-27 11:36:54 -0700

    creates login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-
-  get 'users/index'
-
-  get 'users/new'
+  root 'users#index'
 
   get 'users/' => 'users#index'
   post 'users/' => 'users#create'
   get 'users/new' => 'users#new', as: :new_user
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'login' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

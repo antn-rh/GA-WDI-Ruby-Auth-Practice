14ebe3d0c4acbc06a24d2caab7192d8f03db0599
Antonio Rhee <antoniolrhee@gmail.com>
2016-10-27 10:57:04 -0700

    creates user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+
+  get 'users/new'
+
   get 'users/' => 'users#index'
   post 'users/' => 'users#create'
   get 'users/new' => 'users#new', as: :new_user

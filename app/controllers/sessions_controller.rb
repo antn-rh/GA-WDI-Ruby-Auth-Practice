d39d7d7f2ea0c78aca31105392e6d50a2eae019c
Antonio Rhee <antoniolrhee@gmail.com>
2016-10-27 11:52:32 -0700

    adds create method to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(email: params[:login][:email])
+    if user && user.authenticate(params[:login][:password])
+      # set a cookie, so our browser knows we are who we say we are
+      session[:user_id] = user.id.to_s
+      redirect_to users_path
+    else
+      # give them another shot at logging in
+      # perhaps by redirecting back to the login form
+      render :new
+    end
   end
 
   def destroy
